d76649182b2375832754bc854d0dd782a02421ad
encoderitistiaque <istiaque@encoderit.net>
2021-05-20 17:17:40 +0600

    day-1

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <title>Homepage</title>
+</head>
+
+<body>
+    <!--Scroll Section Start-->
+    <header class="bg-dark">
+        <div class="container">
+            <div class="row bg-dark d-flex align-items-center">
+                <div class="col-lg-2">
+                    <select class="form-select bg-dark form-select-sm mb-0 default-color-white" aria-label="Default select example">
+                        <option selected>MLB</option>
+                        <option value="1">NFL</option>
+                        <option value="2">GOLF</option>
+                    </select>
+
+                </div>
+                <div class="col-lg-10">
+                    <div class="responsive mlb-carousel font-size m-3">
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                        <div class="time-schedule mx-2">
+                            <div>
+                                <p>PHI <span class="float-end">1</span></p>
+                                <p>MIA <span class="float-end">1</span></p>
+                                <p class="text-center">7.10AM</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+    </header>
+    <!--Scroll Section End-->
+
+    <!--Navbar-->
+    <section class="bg-white">
+        <div class="container">
+            <nav class="navbar p-0 navbar-expand-lg navbar-light bg-white">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="image/homepage/logo.png" alt=""></a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact with us</i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fab fa-facebook-square"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fab fa-twitter-square"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fab fa-youtube-square"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <nav class="navbar p-0 border-top navbar-expand-lg navbar-light bg-white">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><i class="fas fa-bars fs-4"></i></a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mb-2 mb-lg-0 me-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    FEATURES
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">HOMEPAGE</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">PAGE TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">POST TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">TYPOGRAPHY</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    FEATURES
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">HOMEPAGE</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">PAGE TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">POST TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">TYPOGRAPHY</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SCORES</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">BUY THEME</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SHOP</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">FOOTBALL</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">BASEBALL</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SCOCCER</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    FEATURES
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">HOMEPAGE</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">PAGE TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">POST TEMPLATES</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">TYPOGRAPHY</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    </section>
+    <!--Navbar End-->
+    <!--Advertise Start -->
+    <section class="default-color-grey">
+        <div class="container">
+            <img class="mx-auto d-block py-3 w-80" src="image/homepage/advertise.jpg" alt="">
+        </div>
+    </section>
+    <!--Advertise End -->
+
+    <!--Hemoitems Start-->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-md-10 col-sm-12 col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <img class="w-100" src="image/homepage/hero-img.jpg" alt="">
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-12">
+                            <img class="w-100" src="image/homepage/hero-img2.jpg" alt="">
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-12">
+                            <img class="w-100" src="image/homepage/hero-img3.jpg " alt="">
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-2 col-md-2 col-sm-12 col-12">
+                    <img class="w-100" src="image/homepage/hero-img3.jpg" alt="">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <!--Hemoitems Start-->
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
